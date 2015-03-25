834e1c072ce67560d4a2303e40ae73bea3c1a39f
ktabori <ktabori@dmngd.co>
2015-03-25 21:49:50 +0100

    Unlock all features

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ popd > /dev/null
 source $THISPATH/Setup/mixins.sh
 
 source $THISPATH/Setup/welcome.sh
-#source $THISPATH/Setup/Run/xcode.sh
-#source $THISPATH/Setup/Run/brew.sh
-#source $THISPATH/Setup/Run/npm.sh
-#source $THISPATH/Setup/Run/ruby.sh
-#source $THISPATH/Setup/Run/heroku.sh
-#source $THISPATH/Setup/Run/apps.sh
+source $THISPATH/Setup/Run/xcode.sh
+source $THISPATH/Setup/Run/brew.sh
+source $THISPATH/Setup/Run/npm.sh
+source $THISPATH/Setup/Run/ruby.sh
+source $THISPATH/Setup/Run/heroku.sh
+source $THISPATH/Setup/Run/apps.sh
 source $THISPATH/Setup/Run/terminal.sh
